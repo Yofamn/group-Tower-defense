09a8ff986560166351c55cd2186c5f07584e4249
Kirin-Qilin <madigan.haselwood@gmail.com>
2025-03-27 15:24:44 -0500

    Created the first POV
    First pov is debugged now

--- Assets/Madigans Folder/Scripts/Top Down.cs
+++ Assets/Madigans Folder/Scripts/Top Down.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace TowerDefense
     {
-    public class ThirdPerson : MonoBehaviour
+    public class TopDown : MonoBehaviour
     {
         // Start is called before the first frame update
         void Start()
