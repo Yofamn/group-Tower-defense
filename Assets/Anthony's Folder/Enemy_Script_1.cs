637b607838665db4ca2b709eda9d8a42f1c1880b
Yofamn <167719121+Yofamn@users.noreply.github.com>
2025-03-27 15:02:43 -0500

    Scripts to make enemy move + SO

--- Assets/Anthony's Folder/Enemy_Script_1.cs
+++ Assets/Anthony's Folder/Enemy_Script_1.cs
@@ -2,17 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_Script_1 : MonoBehaviour
+namespace TowerDefense
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public class Enemy_Script_1 : MonoBehaviour
     {
-        
-    }
+        public Path path;
+        public int index = 0;
 
-    // Update is called once per frame
-    void Update()
-    {
         
+        Enemy_SO enemy_SO;
+        
+
+        void Start()
+        {
+            enemy_SO = GetComponent<Enemy_SO>();
+            path = FindObjectOfType<Path>();
+            StartCoroutine(FollowPath());
+        }
+
+        IEnumerator FollowPath()
+        {
+            Vector3 target;
+            while(path.TryGetPoint(index, out target))
+            {
+                Vector3 start = transform.position;
+
+                float maxDist = Mathf.Min(enemy_SO.Speed * Time.deltaTime, (target - start).magnitude);
+                transform.position = Vector3.MoveTowards(start, target, maxDist);
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(target - start), 0.05f);
+
+                if(transform.position == target) index++;
+                yield return null;
+            }
+            
+            // Uncomment after player/health script is in.
+            
+            //Player player = FindObjectOfType<Player>();
+            //Health.TryDamage(player.gameObject,damage);
+            //Destroy(gameObject);
+        }
     }
 }
