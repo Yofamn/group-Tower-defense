96fb5fd7c54d46d41f6bc773b00bd4a8746902d8
CrazyCase2473 <deacon.mc08@gmail.com>
2025-03-27 14:48:46 -0500

    Created basic health script
    health script made but still not finished, make sure to add what should happen when player and enemys die

--- Assets/Deacons Folder/Health.cs
+++ Assets/Deacons Folder/Health.cs
@@ -2,19 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Health : MonoBehaviour
+namespace TowerDefense{
+    public class Health : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+     public int currentHealth;
+        void TakeDamage(int damage)
+        {
+            currentHealth -= damage;
+            if(currentHealth <= 0)
+            {
+                if(gameObject.CompareTag("Enemy")){
+                    EnemyDeath();
+                }
+                else if(gameObject.CompareTag("Player")){
+                    PlayerDeath();
+                }
+            }
+        }
+        public static void TryDamage(GameObject target, int damage)
+        {
+            Health healthh = target.GetComponent<Health>();
+            if (target != null) {healthh.TakeDamage(damage); }
+        }
+        public void EnemyDeath(){
+            Destroy(gameObject);
+            // death animation or sound
+        }
+        public void PlayerDeath(){
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+            //send to main screen
+        }
 }
 
 // make a health class for tower defense
+}
+
