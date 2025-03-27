ebf392d24b91c0915492e69c4825644abacba3ee
Kirin-Qilin <madigan.haselwood@gmail.com>
2025-03-27 14:13:54 -0500

    wasnt saved

--- Assets/Scripts/First Person.cs
+++ Assets/Scripts/First Person.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FirstPerson : MonoBehaviour
+namespace TowerDefense
 {
-    // Start is called before the first frame update
-    void Start()
+    public class FirstPerson : MonoBehaviour
     {
-        
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
     }
 }
